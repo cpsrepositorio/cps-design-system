dd8d8daf118de94754ab767f4f5a13f4ad47fa91
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2022-03-07 20:13:30 -0300

    build: upgrade vuepress & install plugins

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -60,10 +60,8 @@ module.exports = {
     }
   },
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
   plugins: [
+    '@vuepress/active-header-links',
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
   ]
